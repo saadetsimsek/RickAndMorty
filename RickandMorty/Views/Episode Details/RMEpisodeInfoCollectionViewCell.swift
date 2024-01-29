1b48533080a1da17574a134d13f9822dbb1deed0
Saadet Simsek <saadetsimsek13@gmail.com>
2024-01-29 23:44:25 +0300

    We will create the cells that we will use in Episode Details.

---
+++ RickandMorty/Views/Episode Details/RMEpisodeInfoCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  RMEpisodeInfoCollectionViewCell.swift
+//  RickandMorty
+//
+//  Created by Saadet Şimşek on 29/01/2024.
+//
+
+import UIKit
+
+class RMEpisodeInfoCollectionViewCell: UICollectionViewCell {
+    
+}
